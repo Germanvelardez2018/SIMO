5cf976b9e3cb9e24d5733989011ae44bc2388341
German Velardez <germanvelardez16@gmail.com>
2021-08-26 16:27:37 -0300

    se inicio el repo

---
+++ build/CMakeFiles/FreeRTOS.dir/FreeRTOS-Kernel/portable/GCC/ARM_CM0/port.c.obj.d
@@ -0,0 +1,13 @@
+CMakeFiles/FreeRTOS.dir/FreeRTOS-Kernel/portable/GCC/ARM_CM0/port.c.obj: \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/portable/GCC/ARM_CM0/port.c \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/include/FreeRTOS.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h \
+ /home/gvelardez/tesis/simo_freertos/include/FreeRTOSConfig.h \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/include/projdefs.h \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/include/portable.h \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/include/deprecated_definitions.h \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/portable/GCC/ARM_CM0/portmacro.h \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/include/mpu_wrappers.h \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/include/task.h \
+ /home/gvelardez/tesis/simo_freertos/FreeRTOS-Kernel/include/list.h
